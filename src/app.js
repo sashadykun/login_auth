1a761e69dca9f195c97324a42cf998d64661e54d
SD <sashaluckyman@gmail.com>
2019-04-15 00:08:04 -0700

    complete auth app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/login_form';
 
@@ -9,22 +9,63 @@ const firebaseConfig = require('./db/fire_db_config')
 
 class App extends Component {
 
+    state = { loggedIn: null }
 
 
     componentDidMount() {
         
-        firebase.initializeApp(firebaseConfig)
+        firebase.initializeApp(firebaseConfig);
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        })
+    }
+
+    renderContent(){
+
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Button onPress={() => firebase.auth().signOut()}>
+                                Log Out
+                            </Button>
+                        </CardSection>
+                    </Card>
+                    
+                );
+            case false:
+                return <LoginForm />
+            default:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Spinner size="large" />
+                        </CardSection>
+                    </Card>
+                                       
+                )
+        }
+        
     }
 
 
     render(){
         return (
-            <View>
+            <View >
                 <Header  headerText="Login Page"/>
-                <LoginForm />
+                {this.renderContent()}
             </View>
         )
     }
 }
+
+
 
 export default App;
